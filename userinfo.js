50df02602783f382de952cf82d44d37afb6d3103
Tom A <teang72@gmail.com>
2020-02-01 09:30:09 -0500

    put guild in secrets, got rid of commented example code.

--- userinfo.js
+++ userinfo.js
@@ -4,6 +4,7 @@ const bot = new Discord.Client();
 
 
 const TOKEN = process.env.TOKEN;
+const GUILDID = process.env.GUILDID;
 
 bot.login(TOKEN);
 
@@ -11,21 +12,11 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}`);
   bot.guilds.forEach(guild => console.info(`connected to ${guild.id} : ${guild.name}`));
   
-  const list = bot.guilds.get('672541629172678686');
+  const list = bot.guilds.get(GUILDID);
   list.members.forEach(member => console.info(member));
+  process.exit();
 });
 
-bot.on("message", msg => {
 
-/*
-    if (msg.content.toLowerCase().startsWith("!clearchat")) {
-        async function clear() {
-            msg.delete();
-            const fetched = await msg.channel.fetchMessages({limit: 99});
-            msg.channel.bulkDelete(fetched);
-        }
-        clear();
-    }
-*/
-});
+
 
